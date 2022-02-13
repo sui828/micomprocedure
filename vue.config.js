e88997fdc1d26775bd719abf36bf2bb3f7c215ac
sui828 <hasuya21sui@gmail.com>
2022-02-14 01:21:53 +0900

    なかったことにする

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
-}
-module.exports = {
-    publicPath: '/micomprocedure', //リポジトリ名を指定
-    outputDir: 'docs', //GitHubPagesで公開するにはdistではなくdocs配下にコンパイルする
-}
